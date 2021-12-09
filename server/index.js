4af741afe5800950fefb0208338b83d60d265829
@taras.f <taras.f@360vier.de>
2021-12-09 23:07:12 +0200

    test

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ if(process.env.NODE_ENV !== 'production') require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const PORT = process.env.PORT || 5050
-// const db = require('./db')
+const db = require('./db')
 
 const app = express()
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname,'..', 'client', 'public', 'index.html'));
 });
 
-// const start = async () => {
-//   try {
-//     await db.authenticate()
-//     await db.sync()
-//     app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
-//   } catch (e) {
-//     console.error(e)
-//   }
-// }
+const start = async () => {
+  try {
+    await db.authenticate()
+    await db.sync()
+    app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-// start()
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+start()
+// app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
