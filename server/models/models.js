00ad34df929498e563c42ba277f4c15222b6bd5a
@taras.f <taras.f@360vier.de>
2021-12-13 00:17:17 +0200

    education

--- server/models/models.js
+++ server/models/models.js
@@ -2,11 +2,60 @@ const sequelize = require('../sequelize')
 const {DataTypes, STRING} = require('sequelize')
 
 const Station = sequelize.define('Station', {
-    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    name:{type:DataTypes.STRING, unique:true},
-    address:{type:STRING, unique: true}
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  address: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  tel:{
+    type:DataTypes.STRING
+  },
+  tanks:{
+    type:DataTypes.ARRAY(DataTypes.INTEGER),
+  }
+})
+
+const FuelType = sequelize.define('FuelType', {
+  id:{
+    type:DataTypes.INTEGER, primaryKey: true, autoIncrement:true,
+  },
+  name:{
+    type:STRING, unique:true, allowNull:false
+  },
+  price:{
+    type:DataTypes.INTEGER, allowNull: false,
+  }
+})
+
+const Region = sequelize.define('Region', {
+  id:{
+    type:DataTypes.INTEGER, primaryKey:true, autoIncrement:true
+  },
+  name:{
+    type:DataTypes.STRING, primaryKey:true, allowNull:false, unique:true
+  }
+})
+
+const Tank = sequelize.define('Tank', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  name: {type: DataTypes.STRING},
+  volume:{type:DataTypes.INTEGER},
+  fuelType:{
+    type:DataTypes.INTEGER
+  }
 })
 
 module.exports = {
-    Station
+  Station,
+  Tank,
+  FuelType,
+  Region
 }
