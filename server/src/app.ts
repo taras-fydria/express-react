083935138face2a3c8d1d983f12fdb95fc975770
@taras.f <taras.f@360vier.de>
2022-02-10 00:06:09 +0200

    adding cors to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,18 @@
 import * as dotenv from 'dotenv';
-
-if (process.env.NODE_ENV !== 'production') dotenv.config();
-
 import path from 'path';
 import express, {Request, Response, Application} from 'express';
 import AppRouter from './routes/routes';
 import dbConnection from './dbConnection';
 import errorHandling from "./middleware/ErrorHandling";
+import cors from 'cors'
+
+if (process.env.NODE_ENV !== 'production') dotenv.config();
 
 const PORT = process.env.PORT || 5050;
 
 const app: Application = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/api', AppRouter);
 app.use(express.static(path.resolve(__dirname, '..', '..', 'client', 'dist')));
